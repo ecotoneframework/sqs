8f9330eebbb0d59f5a27de4afd4ccd2d2df58a58
Dariusz <dgafka.mail@gmail.com>
2022-12-27 08:01:59 +0000

    prepare release

---
+++ tests/Behat/Bootstrap/DomainContext.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Test\Ecotone\Sqs\Behat\Bootstrap;
+
+use Behat\Behat\Context\Context;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ */
+class DomainContext extends TestCase implements Context
+{
+}
