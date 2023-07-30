9a361437b37baed4bc1c77ed7cc167e6caed0929
Dariusz <dgafka.mail@gmail.com>
2023-07-30 19:05:39 +0000

    Serializer interceptor (#169)

--- tests/AbstractConnectionTest.php
+++ tests/AbstractConnectionTest.php
@@ -3,12 +3,19 @@
 namespace Test\Ecotone\Sqs;
 
 use Enqueue\Sqs\SqsConnectionFactory;
+use Exception;
 use Interop\Queue\ConnectionFactory;
 use PHPUnit\Framework\TestCase;
 
 abstract class AbstractConnectionTest extends TestCase
 {
     private ?SqsConnectionFactory $connectionFactory = null;
+
+    protected function setUp(): void
+    {
+        self::cleanUpSqs();
+    }
+
 
     public function getConnectionFactory(): ConnectionFactory
     {
@@ -24,5 +31,17 @@ abstract class AbstractConnectionTest extends TestCase
         return new SqsConnectionFactory(
             getenv('SQS_DSN') ? getenv('SQS_DSN') : 'sqs:?key=key&secret=secret&region=us-east-1&endpoint=http://localhost:4576&version=latest'
         );
+    }
+
+    public static function cleanUpSqs(): void
+    {
+        $context = \Test\Ecotone\Sqs\AbstractConnectionTest::getConnection()->createContext();
+
+        foreach (['async', 'sqs'] as $queue) {
+            try {
+                $context->deleteQueue($context->createQueue($queue));
+            } catch (Exception $e) {
+            }
+        }
     }
 }
